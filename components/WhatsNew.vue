4ebc64f65c0d6b6b5ab6e3f364da0a232efa959c
kulvon <satokazuki@outlook.com>
2021-04-12 18:08:11 +0900

    修正51: 感染拡大警報

--- components/WhatsNew.vue
+++ components/WhatsNew.vue
@@ -7,7 +7,7 @@
         </v-icon>
         {{ $t('最新のお知らせ') }}
       </h3>
-      <!-- <span class="WhatsNew-link-to-emergency-page"> -->
+      <span class="WhatsNew-link-to-emergency-page">
         <!-- <v-icon size="20" class="WhatsNew-link-to-emergency-page-icon">
           mdi-bullhorn
         </v-icon>
@@ -15,9 +15,9 @@
           url="https://www.pref.ishikawa.lg.jp/kansen/monitoring.html"
           :label="$t('現在 ステージⅡ（感染拡大警報）')"
         /> -->
-        <!-- <a class="ExternalLink" href="https://www.pref.ishikawa.lg.jp/kansen/monitoring.html" target="_blank">現在 <strong>ステージⅡ（感染拡大注意報）</strong><i aria-label="別タブで開く" role="img" class="v-icon notranslate ExternalLinkIcon mdi mdi-open-in-new theme--light" style="font-size: 15px;"></i>
+        <a class="ExternalLink" href="https://www.pref.ishikawa.lg.jp/kansen/monitoring.html" target="_blank">現在 <strong>ステージⅡ（感染拡大警報）</strong><i aria-label="別タブで開く" role="img" class="v-icon notranslate ExternalLinkIcon mdi mdi-open-in-new theme--light" style="font-size: 15px;"></i>
         </a>
-      </span> -->
+      </span>
     </div>
     <ul class="WhatsNew-list">
       <li v-for="(item, i) in items" :key="i" class="WhatsNew-list-item">
@@ -130,11 +130,11 @@ export default Vue.extend({
 
     .WhatsNew-link-to-emergency-page {
       // 感染拡大警報の色
-      // background-color: rgba(255, 165, 0, 1);
-      // border: 2px solid rgba(255, 165, 0, 1);
+      background-color: rgba(255, 165, 0, 1);
+      border: 2px solid rgba(255, 165, 0, 1);
       // 感染拡大注意報の背景色
-      background-color: rgb(255, 255, 68);
-      border: 2px solid rgb(255, 255, 68);
+      // background-color: rgb(255, 255, 68);
+      // border: 2px solid rgb(255, 255, 68);
       color: #000;
       // color: $gray-2;
       border-radius: 4px;
